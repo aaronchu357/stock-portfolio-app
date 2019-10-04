6aaffe2790eb9e7105219282fa8dc3afeb6268b9
Aaron Chu <aaronchu357@gmail.com>
2019-10-04 15:23:12 -0400

    set up relations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :transactions
+  has_many :stocks, through: :transactions
+
+  has_secure_password
+  validates_uniqueness_of :email
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates :password_digest, confirmation: true
+  validates :password_digest_confirmation, presence: true
 end
