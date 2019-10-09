527f2907124e9c1ac4104c2e64ef086df65f8512
Aaron Chu <aaronchu357@gmail.com>
2019-10-09 12:34:10 -0400

    updated placeholder

--- frontend/src/forms/BuyStockForm.js
+++ frontend/src/forms/BuyStockForm.js
@@ -12,8 +12,9 @@ const BuyStockForm = props => {
 
   return (
     <form onSubmit={handleStockFormSubmit}>
+      Purchase Form:
       <br />
-      <input type="text" name="ticker" placeholder="Ticker" value={ticker} onChange={e => setTicker(e.target.value)} />
+      <input type="text" name="ticker" placeholder="Symbol" value={ticker} onChange={e => setTicker(e.target.value)} />
       <br />
       <input type="number" name="quantity" placeholder="Quantity" value={quantity} min="1" max="10000" onChange={e => setQuantity(e.target.value)} />
       <br />
